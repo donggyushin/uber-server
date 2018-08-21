074a45ac26c20896b5bcece426d01ef09acdec65
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-08-21 17:48:24 +0900

    Place Entity

---
+++ src/entities/Place.ts
@@ -0,0 +1,25 @@
+import { Entity, BaseEntity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, Column } from "typeorm";
+
+@Entity()
+class Place extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type:"text"}) 
+    name: string;
+    @Column({type:"double precision", default:0}) 
+    lat: number;
+    @Column({type:"double precision", default:0}) 
+    lng: number;
+    @Column({type:"text"}) 
+    address: string;
+    @Column({type:"boolean", default:false}) 
+    favorite: boolean;
+
+    @CreateDateColumn()
+    createdAt: string;
+    @UpdateDateColumn()
+    updatedAt: string;
+}
+
+export default Place;
