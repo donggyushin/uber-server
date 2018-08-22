6891ce9a2a6671533e00670b489da96c6c34f55c
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-08-22 12:51:29 +0900

    FacebookConnect Resolver part Three

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -13,9 +13,9 @@ class User extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
     
-    @Column({type:"text", unique:true})
+    @Column({type:"text", nullable:true})
     @IsEmail()
-    email:string;
+    email:string | null;
 
     @Column({type:"boolean", default:false})
     verifiedEmail: boolean;
@@ -26,7 +26,7 @@ class User extends BaseEntity {
     @Column({type: "text"})
     lastName:string;
 
-    @Column({type:"int"})
+    @Column({type:"int", nullable:true})
     age: number;
 
     @Column({type:"text"})
@@ -63,7 +63,7 @@ class User extends BaseEntity {
     lastOrientation: number;
 
     @Column({type:"text", nullable:true})
-    fbID:string;
+    fbId:string;
 
     @ManyToOne(type => Chat, chat => chat.participant)
     chat:Chat;
--- src/types/graphql.d.ts
+++ src/types/graphql.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type Query {\n  sayBye: String!\n  sayHello(name: String!): String!\n}\n\ntype Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  address: String!\n  favorite: Boolean!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Ride {\n  id: Int!\n  status: String!\n  pickUpAddress: String!\n  pickUpLat: Float!\n  pickUpLng: Float!\n  dropOffAddress: String!\n  dropOffLat: Float!\n  dropOffLng: Float!\n  price: Float!\n  duration: String!\n  distance: String!\n  createAt: String!\n  updatedAt: String\n}\n\ntype User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  createdAt: String!\n  updatedAt: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updatedAt: String\n}\n"];
+export const typeDefs = ["type Chat {\n  id: Int!\n  messages: [Message]!\n  participant: [User]!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Message {\n  id: Int!\n  text: String!\n  chat: Chat!\n  user: User!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Query {\n  sayBye: String!\n  sayHello(name: String!): String!\n}\n\ntype Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  address: String!\n  favorite: Boolean!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Ride {\n  id: Int!\n  status: String!\n  pickUpAddress: String!\n  pickUpLat: Float!\n  pickUpLng: Float!\n  dropOffAddress: String!\n  dropOffLat: Float!\n  dropOffLng: Float!\n  price: Float!\n  duration: String!\n  distance: String!\n  createAt: String!\n  updatedAt: String\n  driver: User!\n  passenger: User!\n}\n\ntype FacebookConnectResponse {\n  ok: Boolean!\n  error: String\n  token: String!\n}\n\ntype Mutation {\n  FacebookConnect(firstName: String!, lastName: String!, email: String, fbId: String!): FacebookConnectResponse!\n}\n\ntype User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  createdAt: String!\n  updatedAt: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n  chat: Chat\n  messages: [Message]\n  verifications: [Verification]\n  ridesAsPassengers: [Ride]\n  ridesAsDriver: [Ride]\n  fbID: String\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updatedAt: String\n  user: User!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -10,30 +10,37 @@ export interface SayHelloQueryArgs {
   name: string;
 }
 
-export interface Place {
+export interface Mutation {
+  FacebookConnect: FacebookConnectResponse;
+}
+
+export interface FacebookConnectMutationArgs {
+  firstName: string;
+  lastName: string;
+  email: string | null;
+  fbId: string;
+}
+
+export interface FacebookConnectResponse {
+  ok: boolean;
+  error: string | null;
+  token: string | null;
+}
+
+export interface Chat {
   id: number;
-  name: string;
-  lat: number;
-  lng: number;
-  address: string;
-  favorite: boolean;
+  messages: Array<Message>;
+  participant: Array<User>;
   createdAt: string;
   updatedAt: string | null;
 }
 
-export interface Ride {
+export interface Message {
   id: number;
-  status: string;
-  pickUpAddress: string;
-  pickUpLat: number;
-  pickUpLng: number;
-  dropOffAddress: string;
-  dropOffLat: number;
-  dropOffLng: number;
-  price: number;
-  duration: string;
-  distance: string;
-  createAt: string;
+  text: string;
+  chat: Chat;
+  user: User;
+  createdAt: string;
   updatedAt: string | null;
 }
 
@@ -57,6 +64,12 @@ export interface User {
   lastLng: number | null;
   lastLat: number | null;
   lastOrientation: number | null;
+  chat: Chat | null;
+  messages: Array<Message> | null;
+  verifications: Array<Verification> | null;
+  ridesAsPassengers: Array<Ride> | null;
+  ridesAsDriver: Array<Ride> | null;
+  fbID: string | null;
 }
 
 export interface Verification {
@@ -65,6 +78,36 @@ export interface Verification {
   payload: string;
   key: string;
   used: boolean;
+  createdAt: string;
+  updatedAt: string | null;
+  user: User;
+}
+
+export interface Ride {
+  id: number;
+  status: string;
+  pickUpAddress: string;
+  pickUpLat: number;
+  pickUpLng: number;
+  dropOffAddress: string;
+  dropOffLat: number;
+  dropOffLng: number;
+  price: number;
+  duration: string;
+  distance: string;
+  createAt: string;
+  updatedAt: string | null;
+  driver: User;
+  passenger: User;
+}
+
+export interface Place {
+  id: number;
+  name: string;
+  lat: number;
+  lng: number;
+  address: string;
+  favorite: boolean;
   createdAt: string;
   updatedAt: string | null;
 }
